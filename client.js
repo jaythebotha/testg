d5390fa237c4fa3827232a77440b2c7b279cc98d
jaythebotha <88421234+jaythebotha@users.noreply.github.com>
2024-04-09 22:53:35 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,24 +1,25 @@
+// client.js
 document.getElementById('roblox-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
-  
-    const username = document.getElementById('username').value;
-  
-    try {
-      const response = await fetch('/submit', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: `username=${encodeURIComponent(username)}`
-      });
-  
-      if (!response.ok) {
-        throw new Error('Error sending info to server.');
-      }
-  
-      alert('Info sent to Discord!');
-    } catch (error) {
-      console.error(error);
-      alert('Error sending info to Discord.');
+  event.preventDefault();
+
+  const username = document.getElementById('username').value;
+
+  try {
+    const response = await fetch('/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `username=${encodeURIComponent(username)}`
+    });
+
+    if (!response.ok) {
+      throw new Error('Error sending info to server.');
     }
-  });
+
+    alert('Info sent to Discord!');
+  } catch (error) {
+    console.error(error);
+    alert('Error sending info to Discord.');
+  }
+});
